02db888e93c1df7558f8c5852c9be9cd0e20b367
Philip Cullen <philipcullen93@gmail.com>
2025-03-04 19:56:32 +0000

    Fibonacci added

---
+++ Tutorials/functionsfibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(N):
+    L = []
+    a, b = 0, 1
+    while len(L) < N:
+        a, b = b, a + b
+        L.append(a)
+    return L
+
+ans = fibonacci(10)
+print(f"Fibonacci sequence is {ans}")
